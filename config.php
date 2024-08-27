536e98d8f6ee29e470e0afbbf15f64abdcbd0462
João Vitor Soares da Rosa <70447049+joaovrsoares@users.noreply.github.com>
2024-08-27 10:00:29 -0300

    Configuração em branco

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Conexão ao banco de dados (MySQL)
+define("BD_SERVIDOR", "localhost");
+define("BD_USUARIO", "root");
+define("BD_SENHA", "0000");
+define("BD_BANCO", "0000");
+
+// E-mail para notificação
+define("EMAIL_NOTIFICACAO", "meuemail@gmail.com")
+
+?>
